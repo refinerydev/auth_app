df1ecbb2c4352b4ed6686d1f2d4c533dc33cd65c
hpazk <hpazk.zkh@gmail.com>
2022-08-12 16:33:56 +0700

    create authentication validator and jwt token manager

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,13 @@
+const { PostAuthenticationPayloadSchema } = require("./schema");
+const InvariantError = require("../../exception/InvariantError");
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
